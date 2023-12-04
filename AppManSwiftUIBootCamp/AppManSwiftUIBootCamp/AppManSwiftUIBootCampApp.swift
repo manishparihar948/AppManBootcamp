3998536a9ad8ce2e1a5b2f03c3b9585189b4032b
manishparihar948 <manish.parihar948@gmail.com>
2023-12-04 12:22:50 +0100

    Project Setup

---
+++ AppManSwiftUIBootCamp/AppManSwiftUIBootCamp/AppManSwiftUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppManSwiftUIBootCampApp.swift
+//  AppManSwiftUIBootCamp
+//
+//  Created by Manish Parihar on 04.12.23.
+//
+
+import SwiftUI
+
+@main
+struct AppManSwiftUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
